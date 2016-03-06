6950ebd5f5d77f2fb08f32746f9da51f4daec33b
adirncom <adirncom@ac.sce.ac.il>
2016-03-06 23:23:08 +0200

    fdgsdg

--- CSPSolver/CSPSolver/CSPSolver.py
+++ CSPSolver/CSPSolver/CSPSolver.py
@@ -24,8 +24,10 @@ class CSP(object):
 
 
     #Genetic Algorithm based solution implementation method
-    def SolveGenetic(self):
-        popSize = 1
+    def SolveGenetic(self, retain=0.2, random_select=0.05, mutate=0.01):
+        popSize = 5
+        population = []
+        graded = []
 
         # Generate a population of above state size
         for i in range(0,popSize):
@@ -34,6 +36,53 @@ class CSP(object):
             for varName in individual:
                 for currDom in self.domDict[varName]:
                     individual[varName] = self.getRandomValue(currDom)
+                    print varName, individual[varName]
+
+            population.append(individual)
+            graded.append((self.GlobalValidate(individual), individual))
+
+        # get the individual in order acceding by its grade
+        graded = [ x[1] for x in sorted(graded)]
+        retain_length = int(len(graded)*retain)
+        parents = graded[:retain_length]
+
+        print graded
+        print parents
+
+        # randomly add other individuals to promote genetic diversity
+        for X in graded[retain_length:]:
+            if random_select > random():
+                parents.append(X)
+
+        # mutate some individuals
+        for X in parents:
+            if mutate > random():
+                pos_to_mutate = randint(0, len(X)-1)
+                # this mutation is not ideal, because it
+                # restricts the range of possible values,
+                # but the function is unaware of the min/max
+                # values used to create the individuals,
+                key = X.keys()[pos_to_mutate]
+                for currDom in self.domDict[key]:
+                    X[key] = self.getRandomValue(currDom)
+
+        # crossover parents to create children
+        parents_length = len(parents)
+        desired_length = len(pop) - parents_length
+        children = []
+        while len(children) < desired_length:
+            male = randint(0, parents_length-1)
+            female = randint(0, parents_length-1)
+            if male != female:
+                male = parents[male]
+                female = parents[female]
+                half = len(male) / 2
+                child = male[:half] + female[half:]
+                children.append(child)
+
+        parents.extend(children)
+
+
 
     # Generates a random value from a given domain
     def getRandomValue(self,domain):
@@ -44,21 +93,28 @@ class CSP(object):
         if domain.Ranges and domain.Values:
             rangeOrValue = "Ranges" if randint(1,2) == 1 else "Values"
         elif domain.Ranges:
-            rangeOrValue == "Ranges"
+            rangeOrValue = "Ranges"
 
         if rangeOrValue == "Ranges":
             range = domain.Ranges[randint(0,len(domain.Ranges)-1)]
             val = range[randint(0,len(range)-1)]
         else:
             val = domain.Values[randint(0,len(domain.Values)-1)]
-            print val
 
         return val
 
     # Call all variables validation methods and collect results
-    def GlobalValidate(self):
+    def GlobalValidate(self, individual = None):
+        if (individual == None):
+            individual = self.varDict
+
+        sum = 0
+
         for k,val in self.constraintDict.iteritems():
-           print val.Validate()
+            sum += val.Validate(individual)
+            print k, val.Validate(individual)
+
+        return sum
             
     def Randomize(self):
         pass
@@ -67,18 +123,26 @@ class CSP(object):
         pass
 
 class Constraint(object):
-    def __init__(self, varDict,varValue,funcList=None, checklist=None):
+    def __init__(self, varDict, varKey, funcList=None, checklist=None):
         self.varDict = varDict
-        self.varValue = varValue
+        self.varKey = varKey
         self.funcList = funcList if funcList else dict()
         self.checklist = checklist if checklist else dict()
     
-    def Validate(self):  
+    def Validate(self, individual = None):
+        counter = 0
+        varValue = individual[self.varKey]
+
+        if (individual == None):
+            individual = self.varDict
+
+        # pass over the functions to check (e.g "Different")
         for k,values in self.checklist.iteritems():
+            # pass over the given arguments for the current function (e.g ["NT","SA"])
             for currVal in values:
-                if(self.funcList[k](self.varValue,self.varDict[currVal]) == False):
-                    return False
-        return True
+                if(self.funcList[k](varValue, individual[currVal]) == False):
+                    counter += 1
+        return counter
 
 class Domain(object):
     def __init__(self, Ranges, Values, AttributeInVar):
@@ -110,14 +174,14 @@ a.domDict["NSW"]=   [dom]
 a.domDict["V"]  =   [dom]
 a.domDict["T"]  =   [dom]
 
-diffFuncList = {"Differant": lambda x,y: x!=y}
-a.constraintDict["WA"] = Constraint(a.varDict,a.varDict["WA"],diffFuncList,{"Differant":["NT","SA"]})
-a.constraintDict["NT"] = Constraint(a.varDict,a.varDict["NT"],diffFuncList,{"Differant":["WA","SA","Q"]})
-a.constraintDict["Q"]  = Constraint(a.varDict,a.varDict["Q"],diffFuncList,{"Differant":["NT","SA","NSW"]})
-a.constraintDict["SA"] = Constraint(a.varDict,a.varDict["SA"],diffFuncList,{"Differant":["WA","NT","Q","NSW","V"]})
-a.constraintDict["NSW"]= Constraint(a.varDict,a.varDict["NSW"],diffFuncList,{"Differant":["Q","SA","V"]})
-a.constraintDict["V"]  = Constraint(a.varDict,a.varDict["V"], diffFuncList,{"Differant":["SA","NSW"]})
-a.constraintDict["T"]  = Constraint(a.varDict,a.varDict["T"], diffFuncList)
+diffFuncList = {"Different": lambda x,y: x!=y}
+a.constraintDict["WA"] = Constraint(a.varDict,"WA",diffFuncList,{"Different":["NT","SA"]})
+a.constraintDict["NT"] = Constraint(a.varDict,"NT",diffFuncList,{"Different":["WA","SA","Q"]})
+a.constraintDict["Q"]  = Constraint(a.varDict,"Q",diffFuncList,{"Different":["NT","SA","NSW"]})
+a.constraintDict["SA"] = Constraint(a.varDict,"SA",diffFuncList,{"Different":["WA","NT","Q","NSW","V"]})
+a.constraintDict["NSW"]= Constraint(a.varDict,"NSW",diffFuncList,{"Different":["Q","SA","V"]})
+a.constraintDict["V"]  = Constraint(a.varDict,"V", diffFuncList,{"Different":["SA","NSW"]})
+a.constraintDict["T"]  = Constraint(a.varDict,"T", diffFuncList)
 
 #a.GlobalValidate()
 a.solve("Genetic")
