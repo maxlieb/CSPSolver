1fdde9cd3db81814fc276bddbd041259c76c2eb2
maxlieb <maxzemail@gmail.com>
2016-04-05 19:03:14 +0300

    Merge Fixes and saving

--- CSPSolver/CSPSolver/CSPSolver.py
+++ CSPSolver/CSPSolver/CSPSolver.py
@@ -1,43 +1,76 @@
 from random import randint, random
-class CSP(dict):
+from operator import add
+class CSP(object):
     def __init__(self, varDict, domDict, constraintDict):
         self.varDict = varDict
         self.domDict = domDict
         self.constraintDict = constraintDict
-    
+        self.fitness_history = []
+
     def __init__(self, path):
-        
+
         self.varDict = None
         self.domDict = None
         self.constraintDict = None
-        
+        self.fitness_history = []
         # TODO: Add load data from XML to class Data Members
-    
+
     def __init__(self):
         self.varDict = dict()
         self.domDict = dict()
         self.constraintDict = dict()
+        self.fitness_history = []
 
     def solve(self, algorithm):
         if algorithm == "Genetic":
-            self.SolveGenetic()
+            return self.SolveGenetic()
 
 
     #Genetic Algorithm based solution implementation method
     def SolveGenetic(self, popSize=100):
         pop = self.population(popSize)
-        self.evolve(pop)
+        isSolved = False
+        for _ in xrange(100):
+            pop = self.evolve(pop)
+            for p in pop:
+                self.fitness_history.append(self.GlobalValidate(p))
+            if any([datum == 0 for datum in self.fitness_history]):
+                print "Solution found"
+                isSolved = True
+                break
+        if isSolved:
+            Solution = pop[-1]
+            for k, v in Solution.iteritems():
+                print "{0}:{1}".format(k,v.Color)
+
+            return Solution
+        else:
+            for datum in self.fitness_history:
+                print "**{0}".format([datum])
+
+
+
+    def grade(self,pop):
+        # 'Find average fitness for a population.'
+        summed = reduce(add, (self.GlobalValidate(x) for x in pop), 0)
+        return summed / (len(pop) * 1.0)
 
     def evolve(self, pop, retain=0.2, random_select=0.5, mutate=0.2):
-        graded = [(self.GlobalValidate(x),x) for x in pop]
-        graded = [ x[1] for x in sorted(graded)]
+        graded = []
+        for x in pop:
+            graded.append((self.GlobalValidate(x), x))
+        print "********************"
+
+        graded = [x[1] for x in sorted(graded)]
+        print graded
+        print "********************"
         retain_length = int(len(graded)*retain)
         parents = graded[:retain_length]
+        # for parent in parents:
+        #     for k,v in parent.iteritems():
+        #         print k,v.Color
+        #     print "--------------------------------"
 
-        for parent in parents:
-            for k,v in parent.iteritems():
-                print k,v.Color
-            print "--------------------------------"
 
         # randomly add other individuals to promote genetic diversity
         for X in graded[retain_length:]:
@@ -46,8 +79,8 @@ class CSP(dict):
 
         # mutate some individuals
         for parent in parents:
-            if 1==1:
-            #if mutate > random():
+            # if 1==1:
+            if mutate > random():
                 randomkey = list(parent.keys())[randint(0,len(list(parent.keys()))-1)]
                 for currDom in self.domDict[randomkey]:
                     setattr(parent[randomkey], currDom.AttributeInVar, self.getRandomValue(currDom))
@@ -55,6 +88,8 @@ class CSP(dict):
         # crossover parents to create children
         parents_length = len(parents)
         desired_length = len(pop) - parents_length
+        print "parents_length: {0}".format(parents_length)
+        print "desired_length: {0}".format(desired_length)
 
         children = []
         while len(children) < desired_length:
@@ -63,12 +98,20 @@ class CSP(dict):
             if male != female:
                 male = parents[male]
                 female = parents[female]
-                malehalf = dict(male.items()[len(male)/2:])
-                femalehalf = dict(female.items()[:len(female)/2])
-                child = dict(malehalf).update(femalehalf)
+                # malehalf = dict(male.items()[len(male)/2:])
+                # femalehalf = dict(female.items()[:len(female)/2])
+
+                malehalf = male.items()[len(male)/2:]
+                femalehalf = female.items()[:len(female)/2]
+                malehalf.extend(femalehalf)
+                child = dict(malehalf)
+                #
+                # child = dict(malehalf).update(femalehalf)
                 children.append(child)
+        print "B:Parents Length: {0}".format(len(parents))
+        print "children: {0}".format(children)
         parents.extend(children)
-        print len(parents)
+        print "A:Parents Length: {0}".format(len(parents))
         return parents
 
     def population(self,length):
@@ -89,6 +132,8 @@ class CSP(dict):
 
         # Decide (randomly if possible) if the random value will
         # be selected from a values list of a range list
+        import time
+        time.sleep(0.01)
         rangeOrValue = "Values"
         if domain.Ranges and domain.Values:
             rangeOrValue = "Ranges" if randint(1,2) == 1 else "Values"
@@ -107,34 +152,30 @@ class CSP(dict):
     def GlobalValidate(self, individual = None):
         if (individual == None):
             individual = self.varDict
-
         sum = 0
 
         for k,val in self.constraintDict.iteritems():
+
             sum += val.Validate(individual)
             #print k, val.Validate(individual)
+        if sum ==None:
+            print "From GlobalValidate None found"
 
         return sum
-            
+
     def Randomize(self):
         pass
-        
+
     def Next(self):
         pass
 
-    def __getattr__(self, item):
-        return self[item]
-
-    def __setattr__(self, key, value):
-        self[key] = value
-
-class Constraint(dict):
+class Constraint(object):
     def __init__(self, varDict, varKey, funcList=None, checklist=None):
         self.varDict = varDict
         self.varKey = varKey
         self.funcList = funcList if funcList else dict()
         self.checklist = checklist if checklist else dict()
-    
+
     def Validate(self, individual = None):
         counter = 0
         varValue = individual[self.varKey]
@@ -148,39 +189,24 @@ class Constraint(dict):
             for currVal in values:
                 if(self.funcList[k](varValue, individual[currVal]) == False):
                     counter += 1
+
         return counter
 
-    def __getattr__(self, item):
-        return self[item]
-
-    def __setattr__(self, key, value):
-        self[key] = value
-
-class Domain(dict):
+class Domain(object):
     def __init__(self, Ranges, Values, AttributeInVar):
         self.Ranges = Ranges
         self.Values = Values
         self.AttributeInVar =AttributeInVar
 
-    def __getattr__(self, item):
-        return self[item]
-
-    def __setattr__(self, key, value):
-        self[key] = value
 
 # Create and init the Australian Map CSP
-global a 
+global a
 a = CSP()
 
-class var(dict):
+class var(object):
     def __init__(self, Color):
         self.Color = Color
 
-    def __getattr__(self, item):
-        return self[item]
-
-    def __setattr__(self, key, value):
-        self[key] = value
 # Populate Variables
 a.varDict["WA"] = var(None)
 a.varDict["NT"] = var(None)
@@ -200,7 +226,7 @@ a.domDict["NSW"]=   [dom]
 a.domDict["V"]  =   [dom]
 a.domDict["T"]  =   [dom]
 
-diffFuncList = {"Different": lambda x,y: x!=y}
+diffFuncList = {"Different": lambda x,y: x.Color!=y.Color}
 a.constraintDict["WA"] = Constraint(a.varDict,"WA",diffFuncList,{"Different":["NT","SA"]})
 a.constraintDict["NT"] = Constraint(a.varDict,"NT",diffFuncList,{"Different":["WA","SA","Q"]})
 a.constraintDict["Q"]  = Constraint(a.varDict,"Q",diffFuncList,{"Different":["NT","SA","NSW"]})
@@ -210,9 +236,7 @@ a.constraintDict["V"]  = Constraint(a.varDict,"V", diffFuncList,{"Different":["S
 a.constraintDict["T"]  = Constraint(a.varDict,"T", diffFuncList)
 
 #a.GlobalValidate()
+result = a.solve("Genetic")
 
-# example for saving the results to json file
 from JsonLoader import JsonLoader
-JsonLoader.SaveOutputData(a)
-
-a.solve("Genetic")
+JsonLoader.SaveOutputData(result)
